3305dd09520f2e7cb71810f917228e993c3082f7
Karina Ionkina <kionkina@stuy.edu>
2017-12-08 13:56:46 -0500

    subtractions

--- intro.js
+++ intro.js
@@ -8,16 +8,16 @@ var randomStudent = function(){
 };
 
 
-var changeHeader = function(x){
-    var h = document.getElementById("h");
-    console.log(h);
-    h.innerHTML = x.innerHTML;
-} 
+var fxn = function(x){
+    console.log(document.getElementById("h").innerHTML);    
+}
 
 var student = randomStudent()
 var theList = document.getElementById("thelist");
 theList.innerHTML += "<li>" + student + "</li>";
 
+
 var listItem = document.getElementsByTagName("li");
-listItem.addEventListener("mouseover", changeHeader(listItem));
+console.log(this)
+
 
