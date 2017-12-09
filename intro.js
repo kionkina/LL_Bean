ce0266bc1f396f8f241922cc16c3eda7e28bc1da
TakingTheL <yliu6@stuy.edu>
2017-12-09 14:09:38 -0500

    pretty much done, see bug section at the bottom of intro.js

--- intro.js
+++ intro.js
@@ -4,38 +4,59 @@ var randomStudent = function(){
     return(SL[rannum]);
 };
 
-var changeHeader = function(content){
+var changeHeader = function(){
+    console.log("hover detected!");
     header = document.getElementById("h");
-    header.innerHTML = content;
+    header.innerHTML = this.innerHTML;
+    console.log("header changed");
 };
 
 var addRando = function(){
     var theList = document.getElementById("thelist");
-    theList.innerHTML += "<li>" + randomStudent() + "</li>";
+    var li = document.createElement("li");
+    li.innerHTML = randomStudent();
+    li.addEventListener("mouseover", changeHeader);
+    li.addEventListener("mouseout", resetHeader);
+    li.addEventListener("click", remove);
+    theList.appendChild(li);
 };
 
 var resetHeader = function(){
-    
-    
+    console.log("resetting header");
+    header = document.getElementById("h");
+    header.innerHTML = "Hello World!";
+    console.log("header reset completed");
+}
+
+var remove = function(){
+    console.log("Trying to remove");
+    this.remove();
+    console.log("removed");
 }
 
 var student = randomStudent();
 var theList = document.getElementById("thelist");
+var listItems = document.getElementsByTagName("li");
 theList.innerHTML += "<li>" + student + "</li>";
 
 var button = document.getElementById("b");
 button.addEventListener("click", addRando);
 
-var listItems = document.getElementsByTagName("li");
+
+
 for (var i = 0; i < listItems.length; i ++){
-    listItems[i].addEventListener("mouseover", function() {
-	changeHeader("hi");
-    });
+    listItems[i].addEventListener("mouseover", changeHeader);
+}
+for (var i = 0; i < listItems.length; i ++){
     listItems[i].addEventListener("mouseout", resetHeader);
-    listItems[i].addEventListener("click", function(){
-	console.log("trying to remove");
-	listItems[i].remove(1);
-    });
+}
+for (var i = 0; i < listItems.length; i ++){
+    listItems[i].addEventListener("click", remove);
 };
 
 
+/* BUGS
+
+* header will not reset to "Hello World!" after the last element is removed.
+
+*/
