2ba21ad3dec6c0d7b966e715abc9a1038ede933c
TakingTheL <yliu6@stuy.edu>
2017-12-09 13:45:55 -0500

    progress!

--- intro.js
+++ intro.js
@@ -1,23 +1,41 @@
-
-
 var randomStudent = function(){
     var SL = ["Leo", "Karina", "Adam", "Tmoi", "Khyber", "Alessandro", "Joyce", "Dasha"];
     var rannum = Math.floor(Math.random() * SL.length);
     return(SL[rannum]);
-
 };
 
+var changeHeader = function(content){
+    header = document.getElementById("h");
+    header.innerHTML = content;
+};
 
-var fxn = function(x){
-    console.log(document.getElementById("h").innerHTML);    
+var addRando = function(){
+    var theList = document.getElementById("thelist");
+    theList.innerHTML += "<li>" + randomStudent() + "</li>";
+};
+
+var resetHeader = function(){
+    
+    
 }
 
-var student = randomStudent()
+var student = randomStudent();
 var theList = document.getElementById("thelist");
 theList.innerHTML += "<li>" + student + "</li>";
 
+var button = document.getElementById("b");
+button.addEventListener("click", addRando);
 
-var listItem = document.getElementsByTagName("li");
-console.log(this)
+var listItems = document.getElementsByTagName("li");
+for (var i = 0; i < listItems.length; i ++){
+    listItems[i].addEventListener("mouseover", function() {
+	changeHeader("hi");
+    });
+    listItems[i].addEventListener("mouseout", resetHeader);
+    listItems[i].addEventListener("click", function(){
+	console.log("trying to remove");
+	listItems[i].remove(1);
+    });
+};
 
 
